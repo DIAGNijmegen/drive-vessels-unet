36b506d70dccec82f8c45237efd227163cd0ea59
Kiran Vaidhya <kiranvaidhya93@gmail.com>
2021-10-19 00:42:29 +0200

    removing older version of the algorithm

--- vesselSegmentor/export.bat
+++
@@ -1,3 +0,0 @@
-call .\build.bat
-
-docker save vesselsegmentor > vesselSegmentor.tar
--- vesselSegmentor/export.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-./build.sh
-
-docker save vesselsegmentor | gzip -c > vesselSegmentor.tar.gz
